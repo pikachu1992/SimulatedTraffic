2e4d03bebf5650cfd178257ac1e461f6c37d5cee
pikachu1992 <tmavicente@gmail.com>
2018-06-21 02:05:08 +0100

    add AI traffic

--- SimulatedTraffic/Form1.Designer.cs
+++ SimulatedTraffic/Form1.Designer.cs
@@ -28,13 +28,52 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.txtLog = new System.Windows.Forms.RichTextBox();
+            this.btnGetXpndrAsync = new System.Windows.Forms.Button();
+            this.btnCreateAITraffic = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // txtLog
+            // 
+            this.txtLog.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtLog.Location = new System.Drawing.Point(12, 12);
+            this.txtLog.Name = "txtLog";
+            this.txtLog.Size = new System.Drawing.Size(360, 510);
+            this.txtLog.TabIndex = 1;
+            this.txtLog.Text = "";
+            // 
+            // btnGetXpndrAsync
+            // 
+            this.btnGetXpndrAsync.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnGetXpndrAsync.Location = new System.Drawing.Point(12, 528);
+            this.btnGetXpndrAsync.Name = "btnGetXpndrAsync";
+            this.btnGetXpndrAsync.Size = new System.Drawing.Size(98, 23);
+            this.btnGetXpndrAsync.TabIndex = 2;
+            this.btnGetXpndrAsync.Text = "GetXpndrAsnc";
+            this.btnGetXpndrAsync.UseVisualStyleBackColor = true;
+            this.btnGetXpndrAsync.Click += new System.EventHandler(this.btnGeXpndrAsync_Click);
+            // 
+            // btnCreateAITraffic
+            // 
+            this.btnCreateAITraffic.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnCreateAITraffic.Location = new System.Drawing.Point(116, 528);
+            this.btnCreateAITraffic.Name = "btnCreateAITraffic";
+            this.btnCreateAITraffic.Size = new System.Drawing.Size(98, 23);
+            this.btnCreateAITraffic.TabIndex = 3;
+            this.btnCreateAITraffic.Text = "CreateAITraffic";
+            this.btnCreateAITraffic.UseVisualStyleBackColor = true;
+            this.btnCreateAITraffic.Click += new System.EventHandler(this.btnCreateAITraffic_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(257, 195);
+            this.ClientSize = new System.Drawing.Size(384, 625);
+            this.Controls.Add(this.btnCreateAITraffic);
+            this.Controls.Add(this.btnGetXpndrAsync);
+            this.Controls.Add(this.txtLog);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -42,6 +81,10 @@
         }
 
         #endregion
+
+        private System.Windows.Forms.RichTextBox txtLog;
+        private System.Windows.Forms.Button btnGetXpndrAsync;
+        private System.Windows.Forms.Button btnCreateAITraffic;
     }
 }
 
